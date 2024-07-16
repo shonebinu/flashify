dad9f2206874876c3cce6cbe342a7d63410a3909
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-07-16 16:36:08 +0530

    add favicon

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Flashify</title>
+  <link rel="icon" type="image/x-icon" href="assets/flash-card.png">
   <link rel="stylesheet" href="./styles/main.css">
 </head>
 
