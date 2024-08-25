c9bfd36e20ae22fb3d43494ea7ef4f94df48adb2
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-08-25 16:15:03 +0530

    implement logout

--- app/components/nav.php
+++ app/components/nav.php
@@ -64,7 +64,7 @@ $stringAfterApp = $matches[1];
 
     <hr>
 
-    <a href="" <?php echo $stringAfterApp == "logout" ? 'class=active' : '' ?>>
+    <a href="/app/logout.php" <?php echo $stringAfterApp == "logout" ? 'class=active' : '' ?>>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
         <title>logout</title>
         <path d="M17 7L15.59 8.41L18.17 11H8V13H18.17L15.59 15.58L17 17L22 12M4 5H12V3H4C2.9 3 2 3.9 2 5V19C2 20.1 2.9 21 4 21H12V19H4V5Z" />
