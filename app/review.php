2fa50e600b926b397592ec5fd22af41c5af10ce3
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-09-29 14:48:09 +0530

    chore: add select deck for review page

--- app/review.php
+++ app/review.php
@@ -1,5 +1,6 @@
 <?php
 require_once '../includes/database.php';
+require_once '../includes/app/decks.php';
 
 session_start();
 
@@ -8,6 +9,10 @@ if (!isset($_SESSION['user_id'])) {
   exit;
 }
 
+$db = new Database();
+
+$current_decks = getDecks($_SESSION['user_id'], $search_term = "", $db);
+$selected_deck = $_GET['deck_id'] ?? null;
 ?>
 
 <!DOCTYPE html>
@@ -25,6 +30,31 @@ if (!isset($_SESSION['user_id'])) {
   <?php require_once 'components/nav.php' ?>
   <?php require_once 'components/bubbles.php' ?>
   <main>
+    <section class="section">
+      <h2>Select a Deck</h2>
+      <?php
+      if (empty($current_decks)) {
+        echo "<p>No Decks Available. Create a <a href='decks.php'>deck</a> to access card operations.</p>";
+        exit;
+      }
+      ?>
+      <p><span class="info">To search and navigate, visit <a href="decks.php">decks</a></span></p>
+      <p><span class="info">Sorted based on favorite and created at</span></p>
+      <form>
+        <select class="deck-select" name="deck_id" required onchange="this.form.submit()">
+          <option selected disabled value="">Select a deck</option>
+          <?php
+          foreach ($current_decks as $deck) {
+            $selected = ($selected_deck == $deck['id']) ? 'selected' : '';
+            echo "<option value='{$deck['id']}' $selected>{$deck['name']}</option>";
+          }
+          ?>
+        </select>
+      </form>
+    </section>
+    <section class="section">
+
+    </section>
   </main>
 </body>
 
