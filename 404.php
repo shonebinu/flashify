aa6e902a1f60e3fb40bab610d8caaf5df6c1ef99
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-10-12 19:37:42 +0530

    feat: setup 404 page

---
+++ 404.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Flashify | 404</title>
+  <link rel="icon" type="image/x-icon" href="/assets/flash-card.png">
+  <link rel="stylesheet" href="/styles/404.css">
+</head>
+
+<body>
+  <div>
+    <img src="./assets/flash-card.png" alt="Flashify logo">
+    <h1>Flashify</h1>
+  </div>
+  <div>
+    <h1 class="h404">404</h1>
+    <h2>Oh Shoot! Page not found</h2>
+    <p>Go back to <a href="./index.php">homepage</a></p>
+    <div class="bubbles">
+      <div class="bubble"></div>
+      <div class="bubble"></div>
+      <div class="bubble"></div>
+      <div class="bubble"></div>
+      <div class="bubble"></div>
+      <div class="bubble"></div>
+      <div class="bubble"></div>
+      <div class="bubble"></div>
+      <div class="bubble"></div>
+      <div class="bubble"></div>
+    </div>
+  </div>
+</body>
+
+</html>
